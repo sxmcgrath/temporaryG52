7799be50d05063493ca57af7aa2dd7a2d550f101
Hao <954119351@qq.com>
2020-10-22 10:49:23 +1100

    try merging with main

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,22 @@
-﻿using System.Collections;
+﻿/* 
+    References:
+    - https://www.youtube.com/watch?v=_QajrabyTJc
+    - https://www.youtube.com/watch?v=1o-Gawy3D48
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
+    // Make player speed and jump adjustable in inspector
     [SerializeField] private float walkSpeed = 4.0f, jumpForce = 4.0f;
 
     // Max distance ray can shoot. Shoots from center of player straight down to check if bottom of player is
     // touching ground. (E.g. if center of player to bottom of player is 1.5f, then add .1f to account for slopes)
     [SerializeField] private float jumpRaycastDistance = 1.6f;
-
+    public float fallMult = 2.0f;
+    public float swingMult = 1.5f;
     private Rigidbody rb;
 
     // Set up initial references
@@ -18,6 +26,13 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Update() {
         Jump();
+
+        // If you are falling, increase gravity by fallMult, if you're swinging and going down, increase gravity by swingMult
+        if (rb.velocity.y < 0 && !Input.GetMouseButton(0)) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMult - 1) * Time.deltaTime;
+        } else if (rb.velocity.y < 0 && Input.GetMouseButton(0)) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (swingMult - 1) * Time.deltaTime;
+        }
     }
 
     // Same as Update but works on every physics step
@@ -30,15 +45,24 @@ public class PlayerMovement : MonoBehaviour {
         Vector2 inputDir = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         inputDir.Normalize();
 
-        // Move Player by velocity
+        // Get player velocity based on direction and speed
         Vector3 velocity = new Vector3(inputDir.x, 0.0f, inputDir.y) * walkSpeed * Time.fixedDeltaTime;
-        // Ensure direction is oriented to where player is facing
-        Vector3 newPosition = rb.position + rb.transform.TransformDirection(velocity);
 
-        rb.MovePosition(newPosition);
+        // If player is swinging, player movement will be based on velocity to make swinging smoother.
+        // If player is not swinging, then simply update position.
+        // Note: TransformDirection is to ensure direction is oriented to where player is facing.
+        if (Input.GetMouseButton(0)) {
+            rb.velocity += rb.transform.TransformDirection(velocity);
+        } else {
+            Vector3 newPosition = rb.position + rb.transform.TransformDirection(velocity);
+
+            rb.MovePosition(newPosition);
+        }
+        
+        
     }
 
-    // This implementation may have difficulties with double jumps (mid air jumps)
+    // This implementation may have difficulties with double jumps (mid air jumps). Makes player jump if on ground.
     private void Jump() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             if (isGrounded()) {
--- Assets/Scripts/TongueSwing.cs
+++ Assets/Scripts/TongueSwing.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿/*
+    References:
+    - https://stackoverflow.com/questions/59653787/unity-3d-spring-joint-textures
+    - https://www.reddit.com/r/Unity3D/comments/a77tcl/physics_based_grappling_hook_using_configurable/
+    - https://www.youtube.com/watch?v=Xgh4v1w5DxU&t=1s
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +14,16 @@ public class TongueSwing : MonoBehaviour {
     // Assign GameObjects from Inspector
     public Transform tongueTip, frogViewCam, player;
     public LayerMask grappleableLayers;
+    public GameObject untargettedCursor, targettedCursor;
 
     // Prepare tongue variables
     private LineRenderer lr;
-    private Vector3 grapplePoint, currentGrapplePosition;
+    private Vector3 grapplePoint, originGrapplePosition;
     private SpringJoint joint;
-    private float maxDistance = 100.0f;
+    private float maxDistance = 50.0f, tongueLength;
+    private RaycastHit grappleable;
+    private bool intersecting = false;
+    private GameObject curAttachedObject;
 
     // Set up initial references
     private void Awake() {
@@ -21,9 +32,12 @@ public class TongueSwing : MonoBehaviour {
 
     // Shoot tongue on mouse click and retract tongue on mouse up. (Tongue remains when holding)
     private void Update() {
+        ChangeCursorIfGrappleable();
+
         if (Input.GetMouseButtonDown(0)) {
             ShootTongue();
-            
+        } else if (Input.GetMouseButton(0)) {
+            IntersectTongue();
         } else if (Input.GetMouseButtonUp(0)) {
             RetractTongue();
         }
@@ -31,7 +45,10 @@ public class TongueSwing : MonoBehaviour {
 
     // Late update as we want to draw the tongue after the positions are taken.
     private void LateUpdate() {
-        DrawTongue();
+        // If a joint exists (tongue is stuck to something) draw out the tongue using line renderer
+        if (joint) {
+            DrawTongue();
+        }
     }
 
     // Use Raycast to determine info about which object to shoot tongue at.
@@ -40,7 +57,9 @@ public class TongueSwing : MonoBehaviour {
         // Check if there is an object with a layer that the tongue can stick to.
         if (Physics.Raycast(frogViewCam.position, frogViewCam.forward, out hit, maxDistance, grappleableLayers)) {
             if (hit.collider != null) {
+                curAttachedObject = hit.collider.gameObject;
                 grapplePoint = hit.point;
+
                 joint = player.gameObject.AddComponent<SpringJoint>();
                 joint.autoConfigureConnectedAnchor = false;
                 joint.connectedAnchor = grapplePoint;
@@ -48,32 +67,71 @@ public class TongueSwing : MonoBehaviour {
                 float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
 
                 // The distance range that tongue will stay between
-                joint.maxDistance = distanceFromPoint * 0.4f;
-                joint.minDistance = distanceFromPoint * 0.20f;
+                joint.maxDistance = distanceFromPoint * 0.6f;
+                joint.minDistance = distanceFromPoint * 0.2f;
 
                 // Important properties for feel of grapple
-                joint.spring = 4.5f;
-                joint.damper = 7.0f;
+                joint.spring = 6.0f;
+                joint.damper = 10.0f;
                 joint.massScale = 4.5f;
 
                 lr.positionCount = 2;
-                currentGrapplePosition = tongueTip.position;
+                originGrapplePosition = tongueTip.position;
+            }
+        } 
+    }
+
+    // If aiming at object within maxDistance range and is a grappleable layer, then change cursor to show.
+    private void ChangeCursorIfGrappleable() {
+        if (Physics.Raycast(frogViewCam.position, frogViewCam.forward, out grappleable, maxDistance, grappleableLayers)) {
+            if (grappleable.collider != null) {
+                untargettedCursor.SetActive(false);
+                targettedCursor.SetActive(true);
+            }
+        } else {
+            untargettedCursor.SetActive(true);
+            targettedCursor.SetActive(false);
+        }
+    }
+
+    // While swinging, if tongue intersects with an object, move tongue position to intersection point. Only works if on the same object.
+    // If intersecting with a different object than what the tongue is currently stuck to, then break the tongue.
+    private void IntersectTongue() {
+        RaycastHit intersect;
+        tongueLength = Vector3.Distance(frogViewCam.position, grapplePoint) - 1.0f;
+        Vector3 lineDir = (grapplePoint - tongueTip.position);
+        
+        if (Physics.Raycast(frogViewCam.position, lineDir.normalized, out intersect, tongueLength, grappleableLayers)) {
+            if (intersect.collider != null) {
+                if (curAttachedObject == intersect.collider.gameObject) {
+                    grapplePoint = intersect.point;
+                    joint.connectedAnchor = grapplePoint;
+
+                    intersecting = true;
+                    originGrapplePosition = grapplePoint;
+                } else {
+                    RetractTongue();
+                }
             }
         }
     }
 
     // Draw the tongue from tongueTip to grapplePoint
     private void DrawTongue() {
-        if (!joint) return;
 
-        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
+        if (!intersecting) {
+            originGrapplePosition = Vector3.Lerp(originGrapplePosition, grapplePoint, Time.deltaTime * 6.0f);
+        }
 
         lr.SetPosition(0, tongueTip.position);
-        lr.SetPosition(1, grapplePoint);
+        lr.SetPosition(1, originGrapplePosition);
+
+        
     }
 
     // Stop sticking tongue to object
     private void RetractTongue() {
+        intersecting = false;
         lr.positionCount = 0;
         Destroy(joint);
     }
