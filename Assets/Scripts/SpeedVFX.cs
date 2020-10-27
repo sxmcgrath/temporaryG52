5db68a2bd7eed0da6b2019763ee5934cd15eaa7d
Andrew Zhang <andrew.zhang9799@gmail.com>
2020-10-27 22:23:31 +1100

    Clean script

--- Assets/Scripts/SpeedVFX.cs
+++ Assets/Scripts/SpeedVFX.cs
@@ -6,28 +6,34 @@ public class SpeedVFX : MonoBehaviour {
 
     public Transform speedParticle;
     public Rigidbody rb;
+    private Camera cam;
+
+    void Awake() {
+        cam = GetComponent<Camera>();
+    }
 
     // Update is called once per frame
     void Update() {
-        // Debug.Log(rb.velocity);
-        // Debug.Log(rb.velocity.magnitude);
-        // Debug.Log(rb.velocity);
-        // Debug.Log(rb.gameObject.transform.rotation);
-        // Debug.Log(transform.rotation);
+        addSpeedLines();
+    }
 
+    // Add Speed lines when falling fast enough. If you are moving at sufficient speed but not vertically down, then add
+    // speed lines from direction you are going and increase FOV to make it feel like you are going faster.
+    private void addSpeedLines() {
         if (rb.velocity.magnitude > 12.0f && rb.velocity.y < -14.0f) {
             speedParticle.gameObject.SetActive(true);
             speedParticle.position = transform.position;
-            // speedParticle.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, 0.0f, transform.rotation.w);
+
             speedParticle.rotation = Quaternion.LookRotation(rb.velocity);
             speedParticle.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
-        } else if (rb.velocity.magnitude > 12.0f) {
+        } else if (rb.velocity.magnitude > 12.0f && Mathf.Abs(rb.velocity.x) > 5.0f) {
             speedParticle.gameObject.SetActive(true);
             speedParticle.position = transform.position + transform.forward * 24.0f;
-            // speedParticle.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, 0.0f, transform.rotation.w);
-            speedParticle.rotation = Quaternion.LookRotation(new Vector3(rb.velocity.x, 0.0f, rb.velocity.z));
+            speedParticle.rotation = new Quaternion(transform.rotation.x, transform.rotation.y, 0.0f, transform.rotation.w);
+            // speedParticle.rotation = Quaternion.LookRotation(new Vector3(rb.velocity.x, 0.0f, rb.velocity.z));
+            // speedParticle.rotation = Quaternion.LookRotation(rb.velocity);
             speedParticle.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
-        }else {
+        } else {
             speedParticle.gameObject.SetActive(false);
         }
     }
