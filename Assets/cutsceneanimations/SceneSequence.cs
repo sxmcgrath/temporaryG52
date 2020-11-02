2c1da96a504ea965453d3eb110bc5f753bfb9775
sxmcgrath <63033906+sxmcgrath@users.noreply.github.com>
2020-11-03 01:16:00 +1100

    Cutscene in kitchen

---
+++ Assets/cutsceneanimations/SceneSequence.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSequence : MonoBehaviour
+{
+    public GameObject cutsceneCam;
+    public GameObject playerCam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(TheSequence());   
+    }
+
+    IEnumerator TheSequence(){
+        yield return new WaitForSeconds(6);
+        playerCam.SetActive(true);
+        cutsceneCam.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
